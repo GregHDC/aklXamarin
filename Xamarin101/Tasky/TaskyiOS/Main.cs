ace572cd2d6ad169f4de10312619454a38be6349
Joshua Fenemore <joshuaf@datacom.co.nz>
2015-10-12 23:53:30 +1300

    Updates

--- Xamarin101/Tasky/TaskyiOS/Main.cs
+++ Xamarin101/Tasky/TaskyiOS/Main.cs
@@ -1,16 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Foundation;
-using UIKit;
+// <copyright file="Application.cs" company="Fendustries">
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Original project and code from https://github.com/xamarin/mobile-samples/tree/master/Tasky authored by Bryan Costanich, Craig Dunn
+//
+// Last modified by Joshua Fenemore 11/10/2015
+//
+// </copyright>
 
-namespace Tasky {
-	public class Application {
-		// This is the main entry point of the application.
-		static void Main (string[] args)
+namespace Tasky 
+{
+    using UIKit;
+
+    /// <summary>
+    /// Public class Application. The main entry point for the app.
+    /// </summary>
+	public class Application 
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="args">Runtime arguments</param>
+		static void Main(string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
